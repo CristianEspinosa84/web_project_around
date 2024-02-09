5f931c1b9c4ede1c5cdce2ee5f892bde6bf9a6ba
cristianespinosa84 <cristianespinosasalinas17@gmail.com>
2024-02-09 12:27:17 +1100

    edit readme

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # Tripleten web_project_around
+
+Abre los archivos en tu editor de código y visualízalos en tu navegador para ver el proyecto.
+
+## Características
+
+- Diseño responsivo utilizando Flexbox.
+- Uso de CSS3 para estilos modernos y eficientes.
+- Compatible con todos los navegadores modernos.
+
+## Construido con
+
+- HTML5
+- CSS3 - Uso avanzado de Flexbox para el diseño.
+- Java Script
+
+## Autores
+
+- **Cristian Espinosa** - _Trabajo Inicial_ - [cristian-espinosa] https://cristianespinosa84.github.io/web_project_around/
+
+## Licencia
+
+Este proyecto está abierto al público para su uso y modificación. Siéntete libre de contribuir y compartirlo.
+
+## Agradecimientos
+
+- A todos los que contribuyeron con ideas y soporte.
+- A la comunidad de desarrollo web por recursos y tutoriales.
---
+++ index.js
@@ -0,0 +1,32 @@
+const overlay = document.querySelector(".overlay");
+const profilSelection = document.querySelector(".profile");
+const profileNameElement = document.querySelector(".profile__name");
+const profileAboutElement = document.querySelector(".profile__about");
+const profileEditButton = document.querySelector(".profile__edit");
+const formSection = document.querySelector(".form");
+const formPopup = document.querySelector(".form__popup");
+const inputName = document.querySelector(".form__name");
+const inputAbout = document.querySelector(".form__about");
+const closeButton = formSection.querySelector(".form__close");
+const formButton = document.querySelector(".form__button");
+
+function openPopup() {
+  formSection.classList.add("popup__opened");
+  overlay.classList.add("overlay__visible");
+}
+
+function closeForm() {
+  formSection.classList.toggle("popup__opened");
+  overlay.classList.toggle("overlay__visible");
+}
+
+function formElement(evt) {
+  evt.preventDefault();
+  profileNameElement.textContent = inputName.value;
+  profileAboutElement.textContent = inputAbout.value;
+  closeForm();
+}
+
+profileEditButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closeForm);
+formButton.addEventListener("click", formElement);
